be764bd8040651f22da37c084c64ad7175db560a
wandersalomao <wandersalomao@gmail.com>
2016-05-02 13:21:45 +0100

    Removing my API Key

--- app/src/main/java/com/udacity/nanodegree/wandersalomao/moviebuddy/common/util/Constants.java
+++ app/src/main/java/com/udacity/nanodegree/wandersalomao/moviebuddy/common/util/Constants.java
@@ -6,7 +6,7 @@ package com.udacity.nanodegree.wandersalomao.moviebuddy.common.util;
  */
 public class Constants {
 
-    public static final String API_KEY = "87a901020f496977f9d6d508c5d186ec";
+    public static final String API_KEY = "YOUR_API_KEY";
     public static final String MOVIE_DB_HOST = "http://api.themoviedb.org/3/";
     public static final String POSTER_BASE_URL = "http://image.tmdb.org/t/p/w342/";
     public static final String POSTER_BASE_URL_780 = "http://image.tmdb.org/t/p/w780/";
